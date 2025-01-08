15a009eb7f64c552025436b678b935e4923a2b27
Kostya Betenya <betenya.kostya@mail.ru>
2025-01-08 01:19:21 +0000

    fet: connect file to the project

--- main.h
+++ main.h
@@ -4,4 +4,9 @@
 // general inicializations
 #define DEFAULT_STRING_LENGTH 255;
 
+// work with files
+#define CPU_FILE_PATH "/proc/cpuinfo"
+#define FILE_READ "r"
+#define FILE_WRITE "w"
+
 #endif
--- utils/cpu/cpu.c
+++ utils/cpu/cpu.c
@@ -1,11 +1,14 @@
 #include "cpu.h"
+#include "../../main.h"
+#include "../../source/file_controller/file.h"
 #include <stdio.h>
-
 // /proc/cpuinfo
 // get all info about processor
 CPUInfo *getCpuInfo() {
   CPUInfo *data;
-
+  s_file_controller *ctl = fcCreate(CPU_FILE_PATH, "r");
+  // read data from file
+  fcFreeMemory(ctl);
   return data;
 }
 
