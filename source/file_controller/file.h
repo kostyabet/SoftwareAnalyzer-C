fbda40903765fef09f71821e32d3f1f2260094f0
Kostya Betenya <betenya.kostya@mail.ru>
2025-01-08 00:45:53 +0000

    fet: create file object for work with files

---
+++ source/file_controller/file.h
@@ -0,0 +1,15 @@
+#ifndef FILE_CONTROLLER_H
+#define FILE_CONTROLLER_H
+
+// file struct create
+typedef struct {
+  char name[255];
+} s_file_controller;
+
+// create object
+s_file_controller *CreateNewController(char *file_name);
+
+// free object
+bool fcFreeMemory(s_file_controller *ctl);
+
+#endif
