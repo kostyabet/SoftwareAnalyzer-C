7947a140a8420ce46d6440b0196050a81539c58c
Kostya Betenya <betenya.kostya@mail.ru>
2025-01-08 01:12:19 +0000

    fet: create file open and close functions

--- source/file_controller/file.c
+++ source/file_controller/file.c
@@ -1,16 +1,31 @@
 #include "file.h"
 #include <malloc.h>
+#include <stdio.h>
 
-s_file_controller *fcCreate(char *file_name) {
+// private functions
+FILE *fcOpenDesctiptor(char *name, char *mode) {
+  FILE *descriptor = fopen(name, mode);
+  if (descriptor == NULL)
+    return NULL;
+  return descriptor;
+}
+
+// public functions
+s_file_controller *fcCreate(char *name, char *mode) {
   s_file_controller *ctl = malloc(sizeof(s_file_controller));
   if (ctl == NULL)
     return NULL;
 
-  ctl->name = file_name;
+  ctl->name = name;
+  ctl->mode = mode;
+  ctl->descriptor = fcOpenDesctiptor(ctl->name, ctl->mode);
+  if (ctl->descriptor == NULL)
+    return NULL;
   return ctl;
 }
 
 int fcFreeMemory(s_file_controller *ctl) {
   free(ctl->name);
+  fclose(ctl->descriptor);
   return 1;
 }
--- source/file_controller/file.h
+++ source/file_controller/file.h
@@ -1,13 +1,17 @@
 #ifndef FILE_CONTROLLER_H
 #define FILE_CONTROLLER_H
 
+#include <stdio.h>
+
 // file struct create
 typedef struct {
   char *name;
+  char *mode;
+  FILE *descriptor;
 } s_file_controller;
 
 // create object
-s_file_controller *fcCreate(char *file_name);
+s_file_controller *fcCreate(char *name, char *mode);
 
 // free object
 int fcFreeMemory(s_file_controller *ctl);
